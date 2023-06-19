b6d10ba428b4c8ac332fc10677a6e387200aa9ab
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2023-06-19 12:51:01 +0300

    aaa

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -10,7 +10,7 @@
   // Your apiKey and indexName will be given to you once
   // we create your config
   apiKey: 'ALGOLIA_API_KEY',
-  indexName: 'docsearch',
+  indexName: 'tyk-docs',
   appId: 'ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
   // Replace inputSelector with a CSS selector
   // matching your search input
